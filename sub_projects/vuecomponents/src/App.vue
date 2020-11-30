341a30bc071fa74af970c693802b07a7965eb571
ShoneSingLone <ShoneSingLone@gmail.com>
2020-11-30 17:33:18 +0800

    vue 3.0 tsx

--- sub_projects/vuecomponents/src/App.vue
+++ sub_projects/vuecomponents/src/App.vue
@@ -1,6 +1,17 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+  <HelloWorld
+    msg="Welcome to Your Vue.js + TypeScript App"
+    other="other"
+    :sub="{ msg: 'sub' }"
+  >
+    <template #default="{ args }">
+      <h1>Main slot {{ args }}</h1>
+    </template>
+    <template #sub="{ args, count }">
+      <h1>Sub {{ args }} {{ count }}</h1>
+    </template>
+  </HelloWorld>
 </template>
 
 <script lang="ts">
--- sub_projects/vuecomponents/src/components/HelloWorld.tsx
+++ sub_projects/vuecomponents/src/components/HelloWorld.tsx
@@ -1,25 +1,58 @@
 import { computed, defineComponent, reactive } from "vue";
 
+
+const PrivateComponent = defineComponent({
+  setup(props, { attrs }) {
+    return () => {
+      const sub = attrs.sub as { msg: string };
+      return (
+        <>
+          { sub && sub.msg ? <p>{sub.msg}</p> : <h1>noting</h1>}
+        </>)
+    }
+  }
+});
+
 export default defineComponent({
   name: "HelloWorld",
   props: {
     msg: String
   },
-  setup(props) {
+  data() {
+    return {
+      formData: {
+        input: ""
+      }
+    }
+  },
+  setup(props, ctx) {
+    const { attrs, slots, emit } = ctx;
     /* data */
-    const state = reactive({ count: 1 });
+    const state = reactive({ count: 1, msg: "state" });
     /* computed */
-    const doubleCount = computed(() => state.count * 2);
+    const doubleCount = computed(() => state.count * 2);  /* methods */
     /* methods */
     const handleClick = () => state.count++;
 
     /* render */
     return () => {
+      const
+        Main = slots.default as any,
+        Sub = slots.sub as any,
+        pContent = `click count: ${state.count} computed: ${doubleCount.value}`,
+        mainArgs = { msg: `main's value`, count: state.count };
+
       return (
-        <h1 onClick={handleClick}>
-          {props.msg}
-          {`click count: ${state.count} computed: ${doubleCount.value}`}
-        </h1>
+        <>
+          <h1>Vue 3.0 with JSX</h1>
+          <input v-model={state.count} type="number" />
+          <Main args={mainArgs} />
+          <Sub args={`sub's value`} count={state.count} />
+          <button onClick={handleClick}>click</button>
+          <p>{props.msg}</p>
+          <p>{pContent}</p>
+          <PrivateComponent {...attrs} />
+        </>
       );
     };
   }
